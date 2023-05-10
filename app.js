6a6a4f21fbad541b959b21220588022821ba22ec
Miroslaw Socha <mirek.socha24680@wp.pl>
2023-05-10 15:20:54 +0200

    sekcja branze, kilka poprawek oraz nagłowki sekcji

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const searchIcon = document.querySelector(".search-icon");
 const menu = document.querySelector("#menu");
 const iconToogleMenu = document.querySelector(".icon-toogle-menu");
 let classList = iconToogleMenu.classList;
+const controlsSlider = document.querySelectorAll(".control-slider");
+const slider = document.querySelector(".slider");
+let number = 1;
+
 searchIcon.addEventListener("click", () => {
     if (searchInput.style.display == "block" && searchInput.value == "") {
         searchInput.style.display = "none";
@@ -14,11 +18,11 @@ searchIcon.addEventListener("click", () => {
 iconToogleMenu.addEventListener("click", () => {
     for (let i = 0; i < classList.length; i++) {
         if (classList[i] == "fa-rotate-180") {
-            menu.style.left = "-220px";
+            menu.style.left = "-210px";
             iconToogleMenu.classList.add("fa-rotate-0");
             iconToogleMenu.classList.remove("fa-rotate-180");
             iconToogleMenu.style.color = "#023a5c"
-        }else if (classList[i] == "fa-rotate-0") {
+        } else if (classList[i] == "fa-rotate-0") {
             menu.style.left = "0";
             iconToogleMenu.classList.add("fa-rotate-180");
             iconToogleMenu.classList.remove("fa-rotate-0");
@@ -26,6 +30,31 @@ iconToogleMenu.addEventListener("click", () => {
         }
 
     }
+});
 
 
-})
+
+function currentImage() {
+    let currentImg = number;
+    for (let i = 0; i < controlsSlider.length; i++) {
+        controlsSlider[i].addEventListener("click", (event) => {
+            event.preventDefault();
+            let number = event.target.value;
+            slider.src = `assets/slider/slider${number}.png`;
+            currentImg = number;
+        })
+
+    };
+
+    setInterval(() => {
+        currentImg++;
+        if (currentImg > 4) {
+            currentImg = 1;
+        }
+        slider.src = `assets/slider/slider${currentImg}.png`;
+    }, 3000);
+
+
+}
+
+currentImage();
